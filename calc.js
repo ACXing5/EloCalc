6a09bc1c36b65c877ac13f4757546ce3fc5669d6
ACXing5 <108419087+ACXing5@users.noreply.github.com>
2025-01-02 10:20:42 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -28,6 +28,48 @@ for (item of buttons) {
     })
 }
 
+document.addEventListener("keydown", function(event) {
+    console.log(event.which);
+    if(event.shiftKey==57){
+        event.key = '(';
+    }
+    else if(event.shiftKey==48){
+        event.key = ')';
+    }
+    else if(event.shiftKey==53){
+        event.key = '%';
+    }
+    if(event.keyCode==88){
+        screenValue += '*';
+        screen.value = screenValue;
+    }
+    if(event.key<=9 || event.key=='+' || event.key=='-' || event.key=='*' || event.key=='.' || event.key=='/' || event.key=='%' || event.key=='(' || event.key==')'){
+        screenValue += event.key;
+        screen.value = screenValue;
+    }
+    if(event.keyCode == 13 || event.keyCode == 187)
+    {
+        screen.value = eval(screenValue);
+    }
+    else if(event.keyCode == 46){
+        screenValue = "";
+        screen.value = screenValue;
+        console.clear();
+    }
+    else if(event.keyCode == 8){
+        screenValue = screenValue.slice(0, -1);
+        screen.value = screenValue;
+    }
+    else if(event.keyCode == 67){
+        screenValue = "";
+        screen.value = screenValue;
+        console.clear();
+    }
+    else if(event.keyCode == 82){
+        location.reload();
+    }
+  })
+
   window.onerror = function(){
       alert("PLEASE INPUT VALID EXPRESSION");
       screenValue = "";
